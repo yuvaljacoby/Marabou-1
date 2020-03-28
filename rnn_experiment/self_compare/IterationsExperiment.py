bbb95c2ed909e850258f5bb608b914b358e6f8db
Yuval Jacoby <yuval999@gmail.com>
2020-03-29 00:03:29 +0300

    finish new table code

--- rnn_experiment/self_compare/IterationsExperiment.py
+++ rnn_experiment/self_compare/IterationsExperiment.py
@@ -110,7 +110,7 @@ def run_all_experiments(net_options, points, t_range, other_idx_method, gurobi_p
 
 
 def parse_results_file(name_path_map: Union[List[Tuple[str, str]], str], t_range=range(2, 20), print_latex=False):
-    if isinstance(name_path_map, str):
+    if isinstance(name_path_map, str) and name_path_map.endswith('pkl'):
         os.makedirs("temp/", exist_ok=True)
         results = pickle.load(open(name_path_map, "rb"))
         name_path_map = []
@@ -121,9 +121,20 @@ def parse_results_file(name_path_map: Union[List[Tuple[str, str]], str], t_range
         for k, v in new_files.items():
             pickle.dump(v, open("temp/{}".format(k), "wb"))
             name_path_map.append((k.replace("model_20classes_", ""), "temp/{}".format(k)))
+    if isinstance(name_path_map, str) and os.path.isdir(name_path_map):
+        tuples = []
+        for f in sorted(os.listdir(name_path_map)):
+            p =os.path.join(name_path_map, f)
+            if not os.path.isfile(p):
+                continue
+            m, _ = extract_model_name_ephocs(f)
+            tuples.append((m, p))
+        name_path_map = tuples
 
     x = PrettyTable()
     # names = set([p[0] for p in name_path_map])
+    if len(set(name_path_map)) != len(name_path_map):
+        print('PROBLEM')
     x.field_names = ['Tmax'] + [p[0] for p in name_path_map]  # if p[0] not in names]
 
     rows = [[t] for t in t_range]
@@ -152,19 +163,18 @@ def parse_results_file(name_path_map: Union[List[Tuple[str, str]], str], t_range
             gurobi_time = res['avg_step_time_no_timeout']
             ffnn_time = res['avg_invariant_time_no_timeout'] + res['avg_property_time_no_timeout']
             assert ffnn_time + gurobi_time < avg_run_time, "{}, {}, {}".format(ffnn_time, gurobi_time, avg_run_time)
-            # rows[t - t_range[0]].append("%.2f (%.2f,%.2f) success_rate: %d/%d ffnn_timeout: %d" %
-            #                             (avg_run_time, ffnn_time, gurobi_time, total_success, res['total'],
-            #                              res['len_timeout']))
 
-            rows[t - t_range[0]].append("%.2f (%.2f,%.2f) %d/%d (%d)" % (avg_run_time, ffnn_time, gurobi_time,
-                                                                         total_success, res['total'],
-                                                                         res['len_timeout']))
+            # print("Format is: time (#success/#total) (#timeout)")
+            # rows[t - t_range[0]].append("%.2f (%.2f,%.2f) %d/%d (%d)" % (avg_run_time, ffnn_time, gurobi_time,
+            #                                                              total_success, res['total'],
+            #                                                              res['len_timeout']))
+            rows[t - t_range[0]].append("%.2f (%d/%d)" % (avg_run_time, total_success, res['total']))
 
     for row in rows:
         x.add_row(row)
     # for k, v in rows.items():
     #     x.add_row([k] + ["{}({})".format(v[net_name][0], v[net_name][1]) for net_name in x.field_names])
-    print("Format is: time (#success/#total) (#timeout)")
+    print("Format is: time (#success/#total)")
     print(x)
     if print_latex:
         for t, row in enumerate(rows):
@@ -246,7 +256,9 @@ def parse_inputs(t_range, net_options, points, other_idx_method):
     save_results = True
 
     if sys.argv[1] == 'analyze':
-        parse_results_file(sys.argv[2])
+        parse_results_file(sys.argv[2], t_range)
+    if sys.argv[1] == 'compare':
+        compare_ephocs(sys.argv[2], t_range)
     if sys.argv[1] == 'exp':
         if len(sys.argv) > 2:
             if str.isnumeric(sys.argv[2]):
@@ -268,26 +280,33 @@ def parse_inputs(t_range, net_options, points, other_idx_method):
                                     continue_pickle='')
     exit(0)
 
+def extract_model_name_ephocs(name):
+    m = 'rnn'
+    ephocs=-1
+    fc_count = 0
+    for w in name.split('_')[2:]:
+        if 'rnn' in w:
+            m += w.replace('rnn', '')
+        elif 'fc' in w:
+            fc_count += 1
+        else:
+            m += '_{}fc32'.format(fc_count)
+            ephocs = w.replace('.out', '')
+    return m, ephocs
 
 def compare_ephocs(pkl_dir: str, t_range):
-    def extract_model_name_ephocs(name):
-        m = 'rnn'
-        fc_count = 0
-        for w in name.split('_')[2:]:
-            if 'rnn' in w:
-                m += w.replace('rnn', '')
-            elif 'fc' in w:
-                fc_count += 1
-            else:
-                m += '_{}fc32'.format(fc_count)
-                ephocs = w.replace('.out', '')
-        return m, ephocs
+
 
     models = defaultdict(list)
+    models_to_ephocs = defaultdict(set) # in the ephocs pickle there is timestep, make sure no duplicates
     for f in os.listdir(pkl_dir):
         if not f.endswith('pkl'):
             continue
         m, e = extract_model_name_ephocs(f)
+        if e in models_to_ephocs[m]:
+            print('file {} is duplicate of the ephocs'.format(f))
+            continue
+        models_to_ephocs[m].update(set([e]))
         models[m].append((e, os.path.join(OUT_FOLDER, f)))
     for k, v in models.items():
         print("Results for model: {}".format(k))
@@ -297,35 +316,15 @@ def compare_ephocs(pkl_dir: str, t_range):
         print("\n\n")
 
 if __name__ == "__main__":
-    # parse_results_file([
-    #     ('rnn2_fc0', "pickles/final_gurobi_exps/rnn2_fc0.pkl"),
-    #     ('rnn2_fc1', "pickles/final_gurobi_exps/rnn2_fc1.pkl"),
-    #     ('rnn4_fc1', "pickles/final_gurobi_exps/rnn4_fc1.pkl"),
-
-    #     ('rnn4_fc2', "pickles/final_gurobi_exps/rnn4_fc2.pkl"),
-    #     ('rnn4_rnn2', "pickles/final_gurobi_exps/rnn4_rnn2.pkl"),
-    #     ('rnn4_rnn4_fc4', "pickles/final_gurobi_exps/rnn4_rnn4_fc4.pkl"),
-    #     ('rnn4_rnn4_rnn4_fc1', "pickles/final_gurobi_exps/rnn4_rnn4_rnn4_fc1.pkl"),
-    #     ('rnn8_fc1', "pickles/final_gurobi_exps/rnn8_fc1.pkl"),
-    #
-    # ], print_latex=False)
-    # parse_results_file('gurobi_19night_run2.pkl')
-    # exit(0)
-    net_options = ["./model_20classes_rnn2_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn8_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn8_rnn6_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn16_fc16_epochs200.h5",
-                   "./model_20classes_rnn12_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn4_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn16_fc16_epochs50.h5",
-                   "./model_20classes_rnn10_fc32_fc32_fc32_fc32_fc32_epochs50.h5",
-                   "./model_20classes_rnn6_rnn6_fc32_fc32_fc32_fc32_fc32_epochs50.h5"]
-
-    # other_idx_method = [lambda x: np.argmin(x)]
-
-    other_idx_method = [lambda x: np.argsort(x)[-i] for i in range(2, 7)]
 
     t_range = range(2, 21)
+
+    # parse_results_file('FMCAD_EXP/out_filter/second_filter', t_range)
+    # exit(-1)
+
+    # other_idx_method = [lambda x: np.argmin(x)]
+    other_idx_method = [lambda x: np.argsort(x)[-i] for i in range(2, 7)]
+
     points = pickle.load(open(POINTS_PATH, "rb"))[:5]
 
     gurobi_ptr = partial(AlphasGurobiBased, update_strategy_ptr=Relative_Step, random_threshold=20000,
@@ -335,9 +334,8 @@ if __name__ == "__main__":
 
     # run_all_experiments(['models/AUTOMATIC_MODELS/model_20classes_rnn4_rnn4_fc32_fc320002.ckpt'], points, t_range,
     #                     other_idx_method, gurobi_ptr, steps_num=10)
-    # run_all_experiments([net_options[0]], points, t_range, other_idx_method, gurobi_ptr, steps_num=10)
     exit(0)
-    # other_idx_method = [lambda x: np.argmin(x)]
+
     point = np.array([-1.90037058, 2.80762335, 5.6615233, -3.3241606, -0.83999373, -4.67291775,
                       -2.37340524, -3.94152213, 1.78206783, -0.37256191, 1.07329743, 0.02954765,
                       0.50143303, -3.98823161, -1.05437203, -1.98067338, 6.12760627, -2.53038902,
@@ -345,7 +343,6 @@ if __name__ == "__main__":
                       5.45421917, 4.11029858, -4.65444165, 0.50871269, 1.40619639, -0.7546163,
                       3.68131841, 1.18965503, 0.81459484, 2.36269942, -2.4609835, -1.14228611,
                       -0.28604645, -6.39739288, -3.54854402, -3.21648808])
-    # net = "model_20classes_rnn4_rnn2_fc16_epochs3.h5"
     net = "model_20classes_rnn4_rnn4_rnn4_fc32_epochs50.h5"
     t_range = range(8, 10)
     # gurobi_multi_ptr = partial(AlphasGurobiBasedMultiLayer, update_strategy_ptr=Relative_Step, random_threshold=20000,
