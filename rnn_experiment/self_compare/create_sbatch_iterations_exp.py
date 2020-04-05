5b0a58d5093298456f2621bf377d5fe53340ff6b
Yuval Jacoby <yuval999@gmail.com>
2020-04-05 14:29:20 +0300

    sbatch for list of iterations exp

--- rnn_experiment/self_compare/create_sbatch_iterations_exp.py
+++ rnn_experiment/self_compare/create_sbatch_iterations_exp.py
@@ -19,7 +19,30 @@ def check_if_model_in_dir(model_name: str, output_folder: str):
             return True
     return False
 
-def create_sbatch(models_folder, output_folder, cache_folder=''):
+
+def write_one_sbatch(output_folder: str, model: str):
+    exp_time = str(datetime.now()).replace(" ", "-")
+    with open(os.path.join(output_folder, "run_iterations_exp_" + model + ".sh"), "w") as slurm_file:
+        exp = "iterations".format()
+        model_name = model[:model.rfind('.')]
+        slurm_file.write('#!/bin/bash\n')
+        slurm_file.write('#SBATCH --job-name={}_{}_{}\n'.format(model_name, exp, exp_time))
+        # slurm_file.write(f'#SBATCH --job-name={model}_{exp}_{exp_time}\n')
+        slurm_file.write('#SBATCH --cpus-per-task=2\n')
+        # slurm_file.write(f'#SBATCH --output={model}_{job_output_rel_path}\n')
+        slurm_file.write('#SBATCH --output={}.out\n'.format(os.path.join(OUT_FOLDER, model_name)))
+        # slurm_file.write(f'#SBATCH --partition={partition}\n')
+        slurm_file.write('#SBATCH --time=24:00:00\n')
+        slurm_file.write('#SBATCH --mem-per-cpu=300\n')
+        slurm_file.write('#SBATCH --mail-type=END,FAIL\n')
+        slurm_file.write('#SBATCH --mail-user=yuvalja@cs.huji.ac.il\n')
+        slurm_file.write('#SBATCH -w, --nodelist=hm-47\n')
+        slurm_file.write('export LD_LIBRARY_PATH=/cs/usr/yuvalja/projects/Marabou\n')
+        slurm_file.write('export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou\n')
+        slurm_file.write('. /cs/labs/guykatz/yuvalja/tensorflow/bin/activate\n')
+        slurm_file.write('python3 rnn_experiment/self_compare/IterationsExperiment.py {} {}\n'.format("exp", model))
+
+def create_sbatch(output_folder, models_folder, cache_folder=''):
     print("*" * 100)
     print("creating sbatch {}".format('using cache {}'.format(cache_folder) if cache_folder else ''))
     print("*" * 100)
@@ -29,30 +52,28 @@ def create_sbatch(models_folder, output_folder, cache_folder=''):
 
     os.makedirs(output_folder, exist_ok=1)
     for model in os.listdir(models_folder):
-        exp_time = str(datetime.now()).replace(" ", "-")
-
         if check_if_model_in_dir(model, cache_folder):
             continue
-        with open(os.path.join(output_folder, "run_iterations_exp_" + model + ".sh"), "w") as slurm_file:
-            exp = "iterations".format()
-            model_name = model[:model.rfind('.')]
-            slurm_file.write('#!/bin/bash\n')
-            slurm_file.write('#SBATCH --job-name={}_{}_{}\n'.format(model_name, exp, exp_time))
-            # slurm_file.write(f'#SBATCH --job-name={model}_{exp}_{exp_time}\n')
-            slurm_file.write('#SBATCH --cpus-per-task=2\n')
-            # slurm_file.write(f'#SBATCH --output={model}_{job_output_rel_path}\n')
-            slurm_file.write('#SBATCH --output={}.out\n'.format(os.path.join(OUT_FOLDER, model_name)))
-            # slurm_file.write(f'#SBATCH --partition={partition}\n')
-            slurm_file.write('#SBATCH --time=24:00:00\n')
-            slurm_file.write('#SBATCH --mem-per-cpu=300\n')
-            slurm_file.write('#SBATCH --mail-type=END,FAIL\n')
-            slurm_file.write('#SBATCH --mail-user=yuvalja@cs.huji.ac.il\n')
-            slurm_file.write('#SBATCH -w, --nodelist=hm-47\n')
-            slurm_file.write('export LD_LIBRARY_PATH=/cs/usr/yuvalja/projects/Marabou\n')
-            slurm_file.write('export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou\n')
-            slurm_file.write('source /cs/labs/guykatz/yuvalja/tensorflow/bin/activate.csh\n')
-            slurm_file.write('python3 rnn_experiment/self_compare/IterationsExperiment.py {} {}\n'.format("exp", model))
+        write_one_sbatch(output_folder, model)
+
+def create_sbatch_from_list(output_folder):
+    FMCAD_networks = ['model_20classes_rnn4_rnn4_rnn4_fc32_fc32_fc32_0200.pkl',
+                      'model_20classes_rnn4_rnn4_rnn4_rnn4_fc32_fc32_fc32_0200.pkl',
+                      'model_20classes_rnn8_rnn8_fc32_fc32_0200.pkl',
+                      'model_20classes_rnn12_rnn12_fc32_fc32_fc32_fc32_0200.pkl',
+                      'model_20classes_rnn16_fc32_fc32_fc32_fc32_0100.pkl',
+                      'model_20classes_rnn8_rnn4_rnn4_fc32_fc32_fc32_fc32_0150.pkl']
+
+    for model in FMCAD_networks:
+        write_one_sbatch(output_folder, model)
+
 
 
 if __name__ == '__main__':
-    create_sbatch(sys.argv[1], sys.argv[2], sys.argv[3] if len(sys.argv) >= 4 else '')
+    out_folder = sys.argv[1]
+    if out_folder == 'help':
+        print("out_folder, models_dir, cache_dir (optional)\nIf only out_folder exists use predefine list of models")
+    if len(sys.argv) > 2:
+        create_sbatch(out_folder, sys.argv[2], sys.argv[3] if len(sys.argv) >= 4 else '')
+    else:
+        create_sbatch_from_list(out_folder)
