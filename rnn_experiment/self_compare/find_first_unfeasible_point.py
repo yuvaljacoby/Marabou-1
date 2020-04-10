4f3f3a68fd4e83c03210d1dec122fb42557477b0
Yuval Jacoby <yuval999@gmail.com>
2020-04-10 23:17:37 +0300

    add arguments to find first unfeasible

--- rnn_experiment/self_compare/find_first_unfeasible_point.py
+++ rnn_experiment/self_compare/find_first_unfeasible_point.py
@@ -12,12 +12,7 @@ from polyhedron_algorithms.GurobiBased.GurobiPolyhedronIISBased import GurobiMul
 from rnn_experiment.self_compare.generate_random_points import POINTS_PATH
 
 
-def find_first_point(net_path, points_path, other_idx_method, max_n_iter=10):
-    gurobi_ptr_1 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=1, use_relu=True, add_alpha_constraint=True,
-                           use_counter_example=True, max_steps=1)
-    gurobi_ptr_2 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=2, use_relu=True, add_alpha_constraint=True,
-                         use_counter_example=True, max_steps=15)
-
+def find_first_point(net_path, points_path, other_idx_method, gurobi_ptr_1, gurobi_ptr_2, max_n_iter=10):
     points = pickle.load(open(points_path, "rb"))
     i = 0
     start_point = 10
@@ -30,7 +25,7 @@ def find_first_point(net_path, points_path, other_idx_method, max_n_iter=10):
                 res, queries_stats, alpha_history = adversarial_query(point, 0.01, idx_max, other_idx, net_path,
                                                                       gurobi_ptr_1, n)
                 if not res:
-                    print("finish n=1 UNSAT")
+                    # print("finish n=1 UNSAT")
                     # Polyhedron with dim-1 does not work, check if dim 2 works
                     res, queries_stats, alpha_history = adversarial_query(point, 0.01, idx_max, other_idx, net_path,
                                                                           gurobi_ptr_2, n)
@@ -51,5 +46,28 @@ if __name__ == '__main__':
     other_idx_method = [lambda x: np.argsort(x)[-i] for i in range(2, 7)]
     net_path = sys.argv[1] if len(sys.argv) > 1 else \
         "./FMCAD_EXP/models/model_20classes_rnn8_fc32_fc32_fc32_0050.ckpt"
-    points_path = sys.argv[2] if len(sys.argv) > 2 else POINTS_PATH
-    find_first_point(net_path, points_path, other_idx_method)
+    points_path = POINTS_PATH
+    gurobi_ptr_1 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=1, use_relu=True, add_alpha_constraint=True,
+                           use_counter_example=True, max_steps=1)
+    gurobi_ptr_2 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=2, use_relu=True, add_alpha_constraint=True,
+                         use_counter_example=True, max_steps=15)
+    if len(sys.argv) > 2:
+        if sys.argv[2] == 'random':
+            gurobi_ptr_1 = partial(GurobiMultiLayerRandom, polyhedron_max_dim=1, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True, max_steps=1)
+            gurobi_ptr_2 = partial(GurobiMultiLayerRandom, polyhedron_max_dim=2, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True, max_steps=15)
+        elif sys.argv[2] == 'base':
+            gurobi_ptr_1 = partial(GurobiMultiLayer, polyhedron_max_dim=1, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True)
+            gurobi_ptr_2 = partial(GurobiMultiLayer, polyhedron_max_dim=2, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True)
+        elif sys.argv[2] == 'iis':
+            gurobi_ptr_1 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=1, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True, max_steps=1)
+            gurobi_ptr_2 = partial(GurobiMultiLayerIIS, polyhedron_max_dim=2, use_relu=True, add_alpha_constraint=True,
+                                   use_counter_example=True, max_steps=15)
+        else:
+            raise KeyError()
+
+    find_first_point(net_path, points_path, other_idx_method, gurobi_ptr_1, gurobi_ptr_2)
