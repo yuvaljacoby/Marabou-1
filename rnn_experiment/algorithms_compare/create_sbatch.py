b137e3654f9a690b52b72058a6438d50d714313c
Yuval Jacoby <yuval999@gmail.com>
2020-05-07 16:11:17 +0300

    change rns comperasion to execute on multiple points

---
+++ rnn_experiment/algorithms_compare/create_sbatch.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import os
+import sys
+import shutil
+
+BASE_FOLDER = "/home/yuval/projects/Marabou/"
+if os.path.exists("/cs/usr/yuvalja/projects/Marabou"):
+    BASE_FOLDER = "/cs/usr/yuvalja/projects/Marabou"
+
+#OUT_FOLDER = os.path.join(BASE_FOLDER, "FMCAD_EXP/out/")
+OUT_FOLDER = os.path.join(BASE_FOLDER, "ATVA_EXP/out_rns/")
+os.makedirs(BASE_FOLDER, exist_ok=True)
+os.makedirs(OUT_FOLDER, exist_ok=True)
+
+def check_if_model_in_dir(model_name: str, output_folder: str):
+    if not output_folder:
+        return False
+    for f in os.listdir(output_folder):
+        if model_name in f or model_name[:model_name.rfind('.')] in f:
+            return True
+    return False
+
+
+def write_one_sbatch(output_folder, time):
+    exp_time = str(datetime.now()).replace(" ", "-")
+    with open(os.path.join(output_folder, "exact_time" + str(time) + ".sh"), "w") as slurm_file:
+        slurm_file.write('#!/bin/bash\n')
+        slurm_file.write('#SBATCH --job-name=rns{}_{}\n'.format(time, exp_time))
+        slurm_file.write('#SBATCH --cpus-per-task=2\n')
+        slurm_file.write('#SBATCH --output={}/rns_{}.out\n'.format(OUT_FOLDER, time))
+        slurm_file.write('#SBATCH --time=24:00:00\n')
+        slurm_file.write('#SBATCH --mem-per-cpu=500\n')
+        slurm_file.write('#SBATCH --mail-user=yuvalja@cs.huji.ac.il\n')
+        # slurm_file.write('export LD_LIBRARY_PATH=/cs/usr/yuvalja/projects/Marabou\n')
+        slurm_file.write('export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou\n')
+        slurm_file.write('python3 rnn_experiment/algorithms_compare/iterations_experiment.py {} {}\n'.format("exact", time))
+
+def create_sbatch(output_folder: str, times: int):
+    print("*" * 100)
+    print("creating sbatch")
+    print("*" * 100)
+
+    os.makedirs(output_folder, exist_ok=1)
+    for t in range(2,times):
+        write_one_sbatch(output_folder, t)
+
+if __name__ == '__main__':
+    out_folder = sys.argv[1]
+    if out_folder == 'help':
+        print("out_folder, max_time\ncreates an sbatch file that runs only rns for each time in range(2,max_time)")
+    if len(sys.argv) > 2:
+        create_sbatch(out_folder, int(sys.argv[2]))
